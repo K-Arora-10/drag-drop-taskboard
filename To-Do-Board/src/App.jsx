b550504406566a24b22e1e6e4eaff3388e67196f
Krish <krisharora984@gmail.com>
2025-07-09 04:08:26 +0530

    Route Changes

--- To-Do-Board/src/App.jsx
+++ To-Do-Board/src/App.jsx
@@ -15,7 +15,7 @@ function App() {
     <AuthProvider>
       <Router>
         <Routes>
-          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Navigate to="/dashboard" />} />
           <Route path="/register" element={<Register />} />
           <Route path="/dashboard" element={<PrivateRoute><Dashboard/></PrivateRoute>} />
         </Routes>
